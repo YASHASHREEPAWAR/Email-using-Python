b0927484427dec16bc7c2730aff0a50003195017
YASHASHREEPAWAR <yashashreepawar11@gmail.com>
2022-01-26 23:25:34 +0530

    Create email.py

---
+++ email.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+
+#Create an Email Object
+email = EmailMessage()
+#set the Sender Name
+email['from'] = ' '   #sender name
+#set the receiver address
+email['to'] = ''    #receiver email
+#set email subject
+email['subject'] = ''  #email subject
+#set email content
+email.set_content('')   #main_content
+with smtplib.SMTP(host = 'smtp.gmail.com', port = 587) as smtp:
+    smtp.ehlo() #waking up the server
+    smtp.starttls() #Start Encryption
+    smtp.login('', '') #user login credentials email-id and password
+    smtp.send_message(email) #sending email
+    print("All done!!!")
